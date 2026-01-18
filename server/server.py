6dd2ad1ee48d8fd066df1ab4c2e6fe141b1e613a
YannToukam <yannyvan2005@gmail.com>
2026-01-18 10:21:02 -0500

    server fini

--- server/server.py
+++ server/server.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -17,19 +16,22 @@ app = Flask(__name__)
 CORS(app)
 
 load_dotenv()
+current_problem_context = None
+
 
 @app.route("/compile", methods=["POST"])
 def handle_compile():
     data = request.get_json()
     language = data.get("language")
     user_code = data.get("code")
-    
+
     if not user_code:
         return jsonify({"error": "No code provided"}), 400
 
     result = compile_code_logic(language, user_code)
-    
+
     return jsonify(result)
+
 
 @app.route('/scribe-token', methods=['GET'])
 def get_scribe_token():
@@ -42,31 +44,34 @@ def get_scribe_token():
         print(f"Erreur ElevenLabs : {e}")
         return jsonify({"error": str(e)}), 500
 
+
 @app.route('/process-transcript', methods=['POST'])
 def process_transcript():
+    global current_problem_context
     """Reçoit le texte du frontend et demande une réponse à Gemini"""
     # Extraction sécurisée des données JSON
     data = request.get_json()
     if not data:
         return jsonify({"error": "JSON requis"}), 400
-        
+
     transcript = data.get('transcript')
-    
+
     if transcript:
-        ai_response = generate_ai_logic(transcript)
+        ai_response = generate_ai_logic(transcript, current_problem_context)
         print("AI Response:", ai_response)
         text_to_speech(ai_response)
         return jsonify({"response": ai_response})
-    
-        
+
     return jsonify({"error": "No transcript provided"}), 400
+
 
 @app.route('/api/get-problem/<problem_id>', methods=['GET'])
 def get_problem(problem_id):
+    global current_problem_context
     try:
         print(problem_id)
         print(f"Recherche du problème: {problem_id}")
-        
+
         problem = problems_collection.find_one({
             "$or": [
                 {"question_id": problem_id},      # String
@@ -75,9 +80,10 @@ def get_problem(problem_id):
         })
 
         print(problem)
-        
+
         print(f"Problème trouvé: {problem is not None}")
-        
+        current_problem_context = problem
+
         if problem:
             problem['_id'] = str(problem['_id'])
             return jsonify(problem), 200
@@ -86,6 +92,7 @@ def get_problem(problem_id):
     except Exception as e:
         print(f"Erreur: {str(e)}")
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
